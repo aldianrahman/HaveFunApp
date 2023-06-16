f963eaf97a8ca8590be39f79b98c6685f66feb9c
aldian <yohputra02@gmail.com>
2023-06-16 18:48:09 +0700

    belum membuat ButtonBack 2 Click exit App

--- app/src/main/java/com/example/havefunapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/havefunapp/ui/theme/Color.kt
@@ -2,18 +2,32 @@ package com.example.havefunapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFFF6F61)
-val PurpleGrey80 = Color(0xFFFFC154)
-val Pink80 = Color(0xFFA3DBE1)
+val TextWhite = Color(0xffeeeeee)
+val DeepBlue = Color(0xff06164c)
+val ButtonBlue = Color(0xff505cf3)
+val DarkerButtonBlue = Color(0xff566894)
+val LightRed = Color(0xfffc879a)
+val AquaBlue = Color(0xff9aa5c4)
+val OrangeYellow1 = Color(0xfff0bd28)
+val OrangeYellow2 = Color(0xfff1c746)
+val OrangeYellow3 = Color(0xfff4cf65)
+val Beige1 = Color(0xfffdbda1)
+val Beige2 = Color(0xfffcaf90)
+val Beige3 = Color(0xfff9a27b)
+val LightGreen1 = Color(0xff54e1b6)
+val LightGreen2 = Color(0xff36ddab)
+val LightGreen3 = Color(0xff11d79b)
+val BlueViolet1 = Color(0xffaeb4fd)
+val BlueViolet2 = Color(0xff9fa5fe)
+val BlueViolet3 = Color(0xff8f98fd)
+val Red1 = Color(0xffe57373)
+val Red2 = Color(0xfff44336)
+val Red3 = Color(0xffd32f2f)
+val Blue1 = Color(0xff64b5f6)
+val Blue2 = Color(0xff2196f3)
+val Blue3 = Color(0xff1976d2)
+val Green1 = Color(0xff81c784)
+val Green2 = Color(0xff4caf50)
+val Green3 = Color(0xff388e3c)
 
-val Purple40 = Color(0xFFF3F3F3)
-val PurpleGrey40 = Color(0xFF333333)
-val Pink40 = Color(0xFFFFFFFF)
-
-val primaryColor = Color(0xFFFF6F61) // Merah Muda Terang
-val secondaryColor = Color(0xFFFFC154) // Oranye Terang
-val accentColor = Color(0xFFA3DBE1) // Biru Terang
-
-val backgroundColor = Color(0xFFF3F3F3) // Abu-abu Muda
-val textColor = Color(0xFF333333) // Hitam
-val whiteColor = Color(0xFFFFFFFF) // Putih
+val primaryColor = DeepBlue
